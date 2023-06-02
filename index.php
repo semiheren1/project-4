9b65159fda40e46e6511a8d5e21698f4a0cbd09e
semih <semihheren@outlook.com>
2023-06-02 16:14:03 +0200

    inlog

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<html>
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<div class="hero-image">
+<div class="container">
+    <div class="content">
+        <form method="post" action="index.php">
+            <div class="login-container">
+                <div class="logodiv">
+                    <img class="logo" src="pictures/logo1.png">
+                </div>
+                <div class="logintext">
+                    <label for="username">Naam:</label>
+                    <input type="text" id="username" name="username"><br><br>
+                    <label for="contact">Contact:</label>
+                    <input type="text" id="contact" name="contact"><br><br>
+                    <label for="straatnaam">Straatnaam:</label>
+                    <input type="text" id="straatnaam" name="straatnaam"><br><br>
+                    <label for="postcode">Postcode:</label>
+                    <input type="contact" id="contact" name="contact"><br><br>
+                    <input type="submit" value="Login">
+                </div>
+            </div>
+        </form>
+    </div>
+    </div>
+</div>
+</body>
+</html>
